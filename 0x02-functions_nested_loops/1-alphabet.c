b7c796389661c20f139b0a723d268c81fdd0c12b
Dawit <dwtmekuria@gmail.com>
2022-12-16 09:25:30 -0800

    created c programm that prints a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints small letter alphabet
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	for(int i = 97 ;i < 123; i++ )
+	{
+		printf("%c",i);
+	}
+	return (0);
+}
